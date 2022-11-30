a33e6c4aeedaab944cfce832b083449506dbaa08
Jared Davis <jdavis75@binghamton.edu>
2022-11-30 17:37:44 -0500

    Input Parsing

--- main.cpp
+++ main.cpp
@@ -1,11 +1,40 @@
 #include <vector>
-#include "greedy.h"
+#include "greedy.cpp"
 #include "problem.h"
 #include "solution.h"
-#include "dp.h"
-
+#include "dp.cpp"
+#include <fstream> 
+#include <string>
+#include <iostream>
+#include <boost/algorithm/string.hpp>
 
 int main (int argc, char *argv[]) { 
+    if(argc != 4){
+        cout << "Format Incorrect!" << endl;
+        return -1;
+    }
+
+    ifstream inFile; //input file
+    ofstream outFile; //output file
+
+    inFile.open(argv[1]);
+    outFile.open(argv[2]);
+    int method = stoi(argv[3]);
+    if(!outFile) cout << "output.txt not created"<< endl;
+    int lineWidth = -1;
+    string line = "";
+    vector<int> words;
+
+    //Input Stuff
+    if(inFile.is_open() && inFile.good()){
+        getline(infile, line);
+        lineWidth = stoi(line);
+        getline(infile, line);
+        boost::split(words, line, boost::is_any_of(" "));
+    }
+    inFile.close();
+
+    Problem problem(lineWidth, words);
 
     return 0;
 }
